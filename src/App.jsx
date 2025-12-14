0a625784b30e932c4d46471e6c8d138d8ad8d95a
Akshita <rooh.akshita@gmail.com>
2025-12-14 20:24:36 +0530

    feat: added API calling functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { APP_CONST } from "./constants/app.constant";
+import { ENDPOINTS } from "./constants/endpoints.constant";
 
-console.log(APP_CONST.API_URL());
-
+console.log(`${APP_CONST.API_URL()}${ENDPOINTS.GET_PRODUCTS()}`);
+async function getProducts() {
+  const response = await fetch(
+    `${APP_CONST.API_URL()}${ENDPOINTS.GET_PRODUCTS()}`
+  );
+  const data = await response.json();
+  return data;
+}
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    getProducts().then((data) => setProducts(data));
+  }, []);
+
   return (
     <>
       <div>Hello</div>
-      <div>Hello 2</div>
+      <div>{}</div>
     </>
   );
 }
